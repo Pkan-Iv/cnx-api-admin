759adf8b1d641a40a7294c1b69d8742f66cfa47d
Fabien Erard <fabien.erard@interactiv-group.com>
2020-02-10 15:07:50 +0100

    Remove some packages

--- test/index.js
+++ test/index.js
@@ -1,8 +1,6 @@
 require( '@babel/polyfill' )
 require( '@babel/register' )({
   plugins: [
-    [ '@babel/plugin-proposal-decorators', { 'legacy': true }],
-    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-runtime'
   ],
   presets: [
