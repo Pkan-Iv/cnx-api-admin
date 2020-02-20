12a337360237072682e10e38f22e2a53c756f061
Fabien Erard <fabien.erard@interactiv-group.com>
2020-02-20 15:34:46 +0100

    Fix useDataLoader

---
+++ src/hooks.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import { useDataProvider } from 'react-admin'
+
+export function useDataLoader ({
+  field = 'name',
+  mapper = (row) => ({ id: row.id, name: row.name }),
+  resource = null
+} = {}) {
+  const dataProvider = useDataProvider()
+  const [ error, setError ] = useState( null )
+  const [ loading, setLoading ] = useState( true )
+  const [ values, setValues ] = useState([])
+
+  useEffect( () => {
+    if (loading && resource) {
+      dataProvider.getList( resource, {
+        filter: {},
+        pagination: {
+          page,
+          perPage: 100
+        },
+        sort: {
+          field,
+          order: 'ASC'
+        }
+      }).then( (response) => {
+        const { data } = response
+
+        setLoading( false )
+        setValues( data.map( mapper ))
+      }).catch( (error) => {
+        setError( error )
+        setLoading( false )
+      })
+    }
+  }, [])
+
+  return [ error, loading, values ]
+}
