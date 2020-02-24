254ddf65af69273deb576125fafa8240813ebdec
Fabien Erard <fabien.erard@interactiv-group.com>
2020-02-24 09:55:32 +0100

    Clean code

--- src/hooks.js
+++ src/hooks.js
@@ -16,7 +16,7 @@ export function useDataLoader ({
       dataProvider.getList( resource, {
         filter: {},
         pagination: {
-          page:1,
+          page: 1,
           perPage: 100
         },
         sort: {
