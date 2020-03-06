e02664585a35b3619cb9a7995ce748c5ed861c1d
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-06 14:49:19 +0100

    Handle submit OK

--- src/components/users.js
+++ src/components/users.js
@@ -1,8 +1,9 @@
 import React, { Fragment, useEffect } from 'react'
 
-import { Box, TextField, ListItem } from '@material-ui/core'
+import { Box, Button, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
+import { useFormFields } from '../hooks'
 import { useStore } from '../../lib/hooks'
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +14,27 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 export const Add = ({ actions = { create: null }, id }) => {
-  const classes = useStyles()
+  const classes = useStyles(),
+  [ fields, handleFieldChange ] = useFormFields({
+    login: '',
+    display_name: '',
+    type: '',
+    language: '',
+    project_ref: ''
+  })
+
+  function handleSubmit (e) {
+    const { display_name, language, login, project_ref, type } = fields
+
+    e.preventDefault()
+
+    dispatch({ type: USERS.CREATE.SUCCESS })
+    console.log(display_name)
+    console.log(language)
+    console.log(login)
+    console.log(project_ref)
+    console.log(type)
+  }
 
   return (
     <Fragment>
@@ -23,15 +44,24 @@ export const Add = ({ actions = { create: null }, id }) => {
       <form
         autoComplete='off'
         noValidate
+        onSubmit={ handleSubmit }
       >
         <Box component='span' display='block' height='100%' >
-          <TextField className={classes.input} id='username' label='Username' variant='outlined' margin='normal' />
-          <TextField className={classes.input} id='login' label='Login' variant='outlined' margin='normal' />
-          <TextField className={classes.input} id='type' label='Type' variant='outlined' margin='normal' />
-          <TextField className={classes.input} id='language' label='Language' variant='outlined' margin='normal' />
-          <TextField className={classes.input} id='project' label='Project' variant='outlined' margin='normal' />
-          <TextField className={classes.input} id='whitelabel' label='Whitelabel' variant='outlined' margin='normal' />
+        <TextField className={classes.input} id='login' label='Login' onChange={ handleFieldChange } variant='outlined' margin='normal' />
+          <TextField className={classes.input} id='display_name' label='Username' onChange={ handleFieldChange } variant='outlined' margin='normal' />
+          <TextField className={classes.input} id='type' label='Type' onChange={ handleFieldChange } variant='outlined' margin='normal' />
+          <TextField className={classes.input} id='language' label='Language' onChange={ handleFieldChange } variant='outlined' margin='normal' />
+          <TextField className={classes.input} id='project_ref' label='Project' onChange={ handleFieldChange } variant='outlined' margin='normal' />
         </Box>
+
+        <Button
+            className={ classes.submit }
+            color="primary"
+            type="submit"
+            variant="contained"
+          >
+            SAVE
+          </Button>
       </form>
     </Fragment>
   )
