53aaf9f210202878140691d2a5763f0b8ac56fab
Fabien Erard <fabien.erard@interactiv-group.com>
2020-03-11 17:15:59 +0100

    Enhance webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -97,7 +97,33 @@ module.exports = (env, argv) => {
               {
                 loader: 'babel-loader',
                 options: {
-                  plugins: [],
+                  plugins: [
+                    [ 'babel-plugin-import', {
+                        'libraryName': '@material-ui/core',
+                        'libraryDirectory': 'esm',
+                        'camel2DashComponentName': false
+                      },
+                      'core'
+                    ],
+                    [ 'babel-plugin-import', {
+                        'libraryName': '@material-ui/icons',
+                        'libraryDirectory': 'esm',
+                        'camel2DashComponentName': false
+                      },
+                      'icons'
+                    ],
+                    [ 'babel-plugin-transform-imports', {
+                        '@material-ui/core': {
+                          'transform': '@material-ui/core/esm/${member}',
+                          'preventFullImport': true
+                        },
+                        '@material-ui/icons': {
+                          'transform': '@material-ui/icons/esm/${member}',
+                          'preventFullImport': true
+                        }
+                      }
+                    ]
+                  ],
                   presets: [
                     [ '@babel/preset-env', {
                       corejs: 3,
