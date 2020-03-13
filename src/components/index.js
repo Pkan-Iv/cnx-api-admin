2decc227708b23de42618cbb535184659f35d787
Fabien Erard <fabien.erard@interactiv-group.com>
2020-03-13 16:43:34 +0100

    Fix layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,78 +1,33 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 
 import {
   AppBar,
   Button,
-  Snackbar,
-  Tab,
-  Tabs,
   Toolbar,
   Typography
 } from '@material-ui/core'
 
-import MuiAlert from '@material-ui/lab/Alert'
+import { useStore } from '../../lib/hooks'
 
-import { useDimensions, useStore } from '../../lib/hooks'
+import Auth from './auth'
+import Main from './main'
 
-import Auth  from './auth'
-import Panel from './panel'
-import Panels from './panels'
-import { CREDENTIALS, USER } from '../descriptors'
+import { CREDENTIALS } from '../descriptors'
 
 const useStyles = makeStyles( (theme) => ({
   grow: {
     flexGrow: 1
-  },
-
-  layout: {
-    backgroundColor: theme.palette.background.paper,
-    display: 'flex',
-    flexGrow: 1,
-    height: '100vh'
-  },
-
-  tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`
   }
 }))
 
-function GetTabProps (index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`
-  }
-}
-
 export default function Application () {
   const classes = useStyles(),
-        [ ref, dimensions ] = useDimensions(),
-        [ { action, context, reason }, dispatch ] = useStore(),
-        [ failure, setFailure ] = useState( false ),
-        [ success, setSuccess ] = useState( false ),
-        [ tab, setTab ] = useState( 0 ),
+        [ { context }, dispatch ] = useStore(),
         { authenticated } = context
 
-  function closeSnackBars () {
-    setFailure( false )
-    setSuccess( false )
-  }
-
-  function handleChange (e, index) {
-    setTab( index )
-  }
-
-  function handleClose (event, reason) {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    closeSnackBars()
-  }
-
   function handleLogout () {
-    closeSnackBars()
     dispatch({ type: CREDENTIALS.DELETE.SUCCESS })
   }
 
@@ -87,62 +42,12 @@ export default function Application () {
     )
   }
 
-  function renderPanels () {
-    return Object.keys( Panels ).map( (label, index) => (
-      <Panel key={ label } value={ tab } index={ index }>
-        { tab === index ? Panels[ label ]( dimensions ) : null }
-      </Panel>
-    ))
-  }
-
-  function renderTabs () {
-    return Object.keys( Panels ).map( (label, index) => (
-      <Tab key={ label } label={ label } { ...GetTabProps( index ) } />
-    ))
-  }
-
   function renderView () {
     if (!authenticated)
       return <Auth />
 
-    return (
-      <Fragment>
-        <div className={ classes.layout }>
-          <Tabs className={ classes.tabs } onChange={ handleChange } orientation='vertical' value={ tab } variant='scrollable'>
-            { renderTabs() }
-          </Tabs>
-
-          <div ref={ ref } style={{ width: '100%' }}>
-            { renderPanels() }
-          </div>
-        </div>
-
-        <Snackbar autoHideDuration={ 5000 } open={ success } onClose={ handleClose }>
-          <MuiAlert elevation={6} variant="filled" severity="success" onClose={ handleClose }>
-            Opération réussie avec succès !
-          </MuiAlert>
-        </Snackbar>
-
-        <Snackbar autoHideDuration={ 5000 } open={ failure } onClose={ handleClose }>
-          <MuiAlert elevation={6} variant="filled" severity="error" onClose={ handleClose }>
-            { reason }
-          </MuiAlert>
-        </Snackbar>
-      </Fragment>
-    )
+    return <Main />
   }
-
-  useEffect( () => {
-    if (reason !== '')
-      return setFailure( true )
-
-    switch (action) {
-      case USER.CREATE.SUCCESS:
-      case USER.DELETE.SUCCESS:
-      case USER.UPDATE.SUCCESS:
-        return setSuccess( true )
-    }
-  }, [ action, reason ])
 
   return (
     <Fragment>
---
+++ src/components/main.js
@@ -0,0 +1,118 @@
+import React, { Fragment, useState, useEffect } from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+import {
+  Snackbar,
+  Tab,
+  Tabs
+} from '@material-ui/core'
+
+import MuiAlert from '@material-ui/lab/Alert'
+
+import Panel from './panel'
+import Panels from './panels'
+import { USER } from '../descriptors'
+import { useDimensions, useStore } from '../../lib/hooks'
+
+const useStyles = makeStyles( (theme) => ({
+  grow: {
+    flexGrow: 1
+  },
+
+  layout: {
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    flexGrow: 1,
+    height: '100vh'
+  },
+
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`
+  }
+}))
+
+function GetTabProps (index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`
+  }
+}
+
+export default function Application () {
+  const classes = useStyles(),
+        [ ref, dimensions ] = useDimensions(),
+        [{ action, reason }] = useStore(),
+        [ failure, setFailure ] = useState( false ),
+        [ success, setSuccess ] = useState( false ),
+        [ tab, setTab ] = useState( 0 )
+
+  function closeSnackBars () {
+    setFailure( false )
+    setSuccess( false )
+  }
+
+  function handleChange (e, index) {
+    setTab( index )
+  }
+
+  function handleClose (event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    closeSnackBars()
+  }
+
+  function renderPanels () {
+    return Object.keys( Panels ).map( (label, index) => (
+      <Panel key={ label } value={ tab } index={ index }>
+        { tab === index ? Panels[ label ]( dimensions ) : null }
+      </Panel>
+    ))
+  }
+
+  function renderTabs () {
+    return Object.keys( Panels ).map( (label, index) => (
+      <Tab key={ label } label={ label } { ...GetTabProps( index ) } />
+    ))
+  }
+
+  useEffect( () => {
+    if (reason !== '')
+      return setFailure( true )
+
+    switch (action) {
+      case USER.CREATE.SUCCESS:
+      case USER.DELETE.SUCCESS:
+      case USER.UPDATE.SUCCESS:
+        return setSuccess( true )
+    }
+  }, [ action, reason ])
+
+  return (
+    <Fragment>
+      <div className={ classes.layout }>
+        <Tabs className={ classes.tabs } onChange={ handleChange } orientation='vertical' value={ tab } variant='scrollable'>
+          { renderTabs() }
+        </Tabs>
+
+        <div ref={ ref } style={{ width: '100%' }}>
+          { renderPanels() }
+        </div>
+      </div>
+
+      <Snackbar autoHideDuration={ 5000 } open={ success } onClose={ handleClose }>
+        <MuiAlert elevation={6} variant="filled" severity="success" onClose={ handleClose }>
+          Opération réussie avec succès !
+        </MuiAlert>
+      </Snackbar>
+
+      <Snackbar autoHideDuration={ 5000 } open={ failure } onClose={ handleClose }>
+        <MuiAlert elevation={6} variant="filled" severity="error" onClose={ handleClose }>
+          { reason }
+        </MuiAlert>
+      </Snackbar>
+    </Fragment>
+  )
+}
