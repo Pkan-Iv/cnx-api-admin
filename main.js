411aced438a90bce837cda80073ae9088f4d12e4
Fabien Erard <fabien.erard@interactiv-group.com>
2020-03-17 10:07:09 +0100

    Add a hack on authentification

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline'
 
+import { post_credentials } from './src/actions'
 import Application from './src/components/'
 import Store from './src/store'
 
@@ -30,3 +31,10 @@ ReactDOM.render(
 
   document.getElementById( 'root' )
 )
+
+/*
+Store.dispatch( post_credentials({
+  username: 'connectics',
+  password: 'cnx427!'
+}))
+/**/
