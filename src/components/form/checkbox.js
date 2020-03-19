be37d977e44d559c1b151627edc799a10ecedd16
Fabien Erard <fabien.erard@interactiv-group.com>
2020-03-19 11:21:20 +0100

    Clean checkbox

--- src/components/form/checkbox.js
+++ src/components/form/checkbox.js
@@ -1,53 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-import { Checkbox, FormGroup, FormControlLabel } from '@material-ui/core'
-
-import { useStore } from 'lib/hooks'
+import { Checkbox } from '@material-ui/core'
 
 export default function FormCheckbox({
-  dataHandler = null,
+  checked = false,
   label = '',
-  onChange,
-  value = ''
+  onChange
 } = {}) {
-  const [ checked, setChecked ] = useState({
-    create: '',
-    read: '',
-    update: '',
-    delete: ''
-  })
-  const values = dataHandler()
-
-  function renderValues (label) {
-    console.log(values)
-    return values.map( (value) => (
-      console.log(value.label)
-      /* <Checkbox
-            checked={ value.label }
-            label={ label }
-            onChange={ handleCheck }
-          /> */
-    ))
-  }
-
-
-  const handleCheck = (label) => {
-    return (e) =>{
-    setChecked({...checked, [e.target.label]: e.target.checked})
-    }
-  }
-
-  if (values === undefined) {
-    return null
-  }
-
-  renderValues()
-
   return (
-    <FormGroup>
-      <FormControlLabel
-        />
-
-    </FormGroup>
+    <Checkbox checked={ checked }
+      onChange={ onChange }
+      value='primary'
+      inputProps={{ 'aria-label': 'primary checkbox' }}
+    />
   )
 }
