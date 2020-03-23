b04e6768a538fc095eb88fdc84f9d278caa7501f
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-23 15:39:22 +0100

    Email sent with nodemailer

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
+import nodemailer from 'nodemailer'
 
 import * as Config from '../config.json'
 
 const app = express(),
       { server } = Config,
-      { PORT } = server
+      { PORT, pass, user } = server
 
 app.use( cors() )
 
@@ -16,6 +17,24 @@ app.use(
   })
 )
 app.use( bodyParser.json )
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: user,
+    pass: pass
+  }
+}),
+mailOptions = {
+  from: user,
+  to: 'patrick.kaninda@interactiv-group.com',
+  subject: 'Sending Email using Node.js, Expres and nodemailer',
+  text: 'Test 01'
+}
+
+transporter.sendMail ( mailOptions, (error, info) => {
+  (error) ? console.log(error) : console.log(`Email sent: ${ info.response }`)
+})
 
 app.listen(PORT, () =>
   console.log(`App is listening on port ${PORT}`)
