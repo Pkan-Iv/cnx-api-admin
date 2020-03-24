e45225eeb074d77659933ba302e36a2b7636d7a7
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-24 12:06:14 +0100

    Clean code

--- server/controller/email.js
+++ server/controller/email.js
@@ -30,7 +30,7 @@ export default {
 
     const sendEMail = () => {
       transporter.sendMail(mailOptions, (error, info) => {
-        (error) ? res.status(500).json(`An error occured while sending the email`) : res.status(200).json({ message: `** Email sent: ${info.response} **` })
+        (error) ? res.status(500).json(`An error occured while sending the email, ${error}`) : res.status(200).json({ message: `** Email sent: ${info.response} **` })
       })
     }
     sendEMail()
