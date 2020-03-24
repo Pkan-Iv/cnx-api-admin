f6e640d2c6b1b15bda51a422a57ceb8db8febca0
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-24 12:08:12 +0100

    Sent email from UI

--- src/components/auth.js
+++ src/components/auth.js
@@ -12,6 +12,12 @@ import { patch_credentials, post_credentials } from '../actions'
 import { useStore } from 'lib/hooks'
 // import GoogleLoginButton from './google'
 
+import * as Config from '../../config.json'
+import fetch from 'cross-fetch'
+
+const { server } = Config,
+      { HOST, PORT } = server
+
 const useStyles = makeStyles(theme => ({
 
   paper: {
@@ -38,17 +44,20 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
+
 export default function Auth () {
-  const classes = useStyles(),
-        [ fields, setFields ] = useState({
+  const [ fields, setFields ] = useState({
           username: '',
           password: ''
         } ||
         { username: '',
           password: '',
           password2: ''
+        }||
+        { email: ''
         }),
         [ forgotten, setForgotten ] = useState( false ),
+        [ userEmail, setEmail ] = useState( false ),
         [ state, dispatch ] = useStore()
 
   function confirmPassword () {
@@ -62,8 +71,31 @@ export default function Auth () {
     }
   }
 
-  function handleReset() {
+  function handleRecover(e) {
+    const { email } = fields
+    const headersConfig = {
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type': 'application/json',
+      Accept: '*/*'
+    }
+    const request = { email: email}
+    e.preventDefault()
+
+    if (email !== undefined) {
+      fetch(`${HOST}:${PORT}/api/pra/reset`, {
+        headers: headersConfig,
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(request)
+      })
+      .then( (response) => response.json())
+      .catch( (err) => console.log('error', err))
+    }
     setForgotten( !forgotten )
+  }
+
+  function handleReset() {
+    setEmail( !userEmail )
   }
 
   function handleSubmit (e) {
@@ -74,123 +106,157 @@ export default function Auth () {
   }
 
   function handleSubmitReset (e) {
-    const { password, password2, username } = fields
+    const { password, username } = fields
 
     e.preventDefault()
     confirmPassword() ? dispatch(patch_credentials({ password, username })) : console.log('Try again')
   }
 
   return (
-    (
-    !forgotten
-    ? <Paper className={ classes.paper }>
-          <form className={ classes.form } onSubmit={ handleSubmit }>
-            <TextField
-              autoFocus
-              fullWidth
-              id="username"
-              label="Username"
-              margin="normal"
-              name="username"
-              onChange={ createChangeHandler('username') }
-              required
-              variant="outlined"
-            />
+    ( !userEmail && !forgotten)
+    ? (<Signin
+        handleSubmit={handleSubmit}
+        createChangeHandler={createChangeHandler}
+        handleReset={handleRecover} />)
+    : ( !userEmail && forgotten )
+    ? <RecoverPassword
+        handleRecover={handleRecover}
+        createChangeHandler={createChangeHandler} />
+    : (<UpdatePassword
+        handleSubmitReset={handleSubmitReset}
+        createChangeHandler={createChangeHandler}
+        handleReset={handleReset} />)
+  )
+}
 
-            <TextField
-              fullWidth
-              id="password"
-              label="Password"
-              margin="normal"
-              name="password"
-              onChange={ createChangeHandler('password') }
-              required
-              autoComplete='no'
-              type="password"
-              variant="outlined"
-            />
+function RecoverPassword({ createChangeHandler, handleRecover}) {
+  const classes = useStyles()
+  return (
+    <Paper className={classes.paper}>
+      <form className={classes.form}>
+        <TextField
+          autoFocus
+          fullWidth
+          id='email'
+          label='Email'
+          margin='normal'
+          name='email'
+          onChange={createChangeHandler('email')}
+          required variant='outlined' />
 
-            <Box component='div' className={ classes.button }  >
-              <Button
-                className={ classes.submit }
-                color="primary"
-                type="submit"
-                style={{'float': 'left'}}
-                variant="contained">
-                Sign In
-              </Button>
-              <Button
-                color="secondary"
-                onClick={ handleReset }
-                style={{'float': 'right'}}
-                variant="contained">
-                RESET PASSWORD
-              </Button>
+        <Box component='div' className={classes.button}>
+          <Button color='primary'
+            onClick={handleRecover}
+            style={{'float': 'right'}} variant='contained'>
+            SEND EMAIL
+          </Button>
+        </Box>
+      </form>
+    </Paper>
+  )
+}
 
-              {
-                /*<GoogleLoginButton className={ classes.submit } type='submit'/>*/
-              }
-            </Box>
-          </form>
-        </Paper>
-    : <Paper className={ classes.paper }>
-    <form className={ classes.form } onSubmit={ handleSubmitReset }>
-      <TextField
-        autoFocus
-        fullWidth
-        id="username"
-        label="Username"
-        margin="normal"
-        name="username"
-        onChange={ createChangeHandler('username') }
-        required
-        variant="outlined"
-      />
+function Signin({handleSubmit, createChangeHandler, handleReset}) {
+  const classes = useStyles()
+  return (
+    <Paper className={classes.paper}>
+      <form className={classes.form} onSubmit={handleSubmit}>
+        <TextField
+          autoFocus
+          fullWidth
+          id='username'
+          label='Username'
+          margin='normal'
+          name='username'
+          onChange={createChangeHandler('username')}
+          required variant='outlined' />
 
-      <TextField
-        fullWidth
-        id="password"
-        label="Password"
-        margin="normal"
-        name="password"
-        onChange={ createChangeHandler('password') }
-        required
-        autoComplete='no'
-        type="password"
-        variant="outlined"
-      />
-      <TextField
-        fullWidth
-        id="password2"
-        label="Password2"
-        margin="normal"
-        name="password2"
-        onChange={ createChangeHandler('password2') }
-        required
-        autoComplete='no'
-        type="password"
-        variant="outlined"
-      />
+        <TextField
+          fullWidth
+          id='password'
+          label='Password'
+          margin='normal'
+          name='password'
+          onChange={createChangeHandler('password')}
+          required autoComplete='no'
+          type='password'
+          variant='outlined' />
 
-      <Box component='div' className={ classes.button }  >
-        <Button
-          color="primary"
-          type="submit"
-          style={{'float': 'left'}}
-          variant="contained">
-          RESET
-        </Button>
-        <Button
-          color="secondary"
-          onClick={ handleReset }
-          style={{'float': 'right'}}
-          variant="contained">
-          CANCEL
-        </Button>
+        <Box component='div' className={classes.button}>
+          <Button className={classes.submit}
+            color='primary'
+            type='submit'
+            style={{'float': 'left'}}
+            variant='contained'>
+            Sign In
+          </Button>
+          <Button color='secondary'
+            onClick={handleReset}
+            style={{'float': 'right'}} variant='contained'>
+            RESET PASSWORD
+          </Button>
 
-      </Box>
-    </form>
-  </Paper>
-    )
+          { /*<GoogleLoginButton className={ classes.submit } type='submit'/>*/ }
+        </Box>
+      </form>
+    </Paper>
+  )
+}
+
+function UpdatePassword({handleSubmitReset, createChangeHandler, handleReset}) {
+  const classes = useStyles()
+  return (
+    <Paper className={classes.paper}>
+      <form className={classes.form} onSubmit={handleSubmitReset}>
+        <TextField
+          autoFocus
+          fullWidth
+          id='username'
+          label='Username'
+          margin='normal'
+          name='username'
+          onChange={createChangeHandler('username')}
+          required
+          variant='outlined' />
+
+        <TextField
+          fullWidth
+          id='password'
+          label='Password'
+          margin='normal'
+          name='password'
+          onChange={createChangeHandler('password')}
+          required autoComplete='no'
+          type='password'
+          variant='outlined' />
+        <TextField
+          fullWidth
+          id='password2'
+          label='Password2'
+          margin='normal'
+          name='password2'
+          onChange={createChangeHandler('password2')}
+          required autoComplete='no'
+          type='password'
+          variant='outlined' />
+
+        <Box component='div' className={classes.button}>
+          <Button
+            color='primary'
+            type='submit'
+            style={{'float': 'left'}}
+            variant='contained'>
+            RESET
+          </Button>
+          <Button
+            color='secondary'
+            onClick={handleReset}
+            style={{'float': 'right'}}
+            variant='contained'>
+            CANCEL
+          </Button>
+        </Box>
+      </form>
+    </Paper>
   )
 }
