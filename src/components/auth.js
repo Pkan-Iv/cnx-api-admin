7dd42211e5f0762de957c94270cac22b6c8cd393
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-25 10:58:10 +0100

    Clean code

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,20 +1,15 @@
-import React, { useState } from 'react'
+import React, { Fragment } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 
-import {
-  Box,
-  Button,
-  Paper,
-  TextField
-} from '@material-ui/core'
 
 import { post_forgotten_credentials } from '../actions'
 import { useStore } from 'lib/hooks'
 // import GoogleLoginButton from './google'
 
 
-import { ResetPassword } from './reset'
 import { ForgottenPassword } from './forgottenPassword'
+import { ResetPassword } from './reset'
+import { Signin } from './Signin'
 
 export const useStyles = makeStyles(theme => ({
 
@@ -44,95 +39,27 @@ export const useStyles = makeStyles(theme => ({
 
 
 export default function Auth () {
-  const [ fields, setFields ] = useState({
-          username: '',
-          password: ''
-        } ),
-        [ { context }, dispatch ] = useStore(),
+  const [ { context } ] = useStore(),
         { forgotten, userEmail } = context
 
-  function createChangeHandler (field) {
-    return (e) => {
-      setFields({ ...fields, [ field ]: e.target.value })
+
+  function renderView() {
+    if (!forgotten){
+     return <Signin />
     }
-  }
-
-  function handleForgotten() {
-    dispatch({ type: CREDENTIALS.FORGOTTEN.SUCCESS })
-    // dispatch(post_forgotten_credentials())
-  }
-
-  function handleSubmit (e) {
-    const { password, username } = fields
-
-    e.preventDefault()
-    dispatch(post_credentials({ password, username }))
+    if( !userEmail ){
+      return<ForgottenPassword />
+      }
+    return <ResetPassword />
   }
 
 
   console.log(userEmail, forgotten)
   return (
-    ( !userEmail && !forgotten)
-    ? (<Signin
-        handleSubmit={handleSubmit}
-        createChangeHandler={createChangeHandler}
-        handleForgotten={handleForgotten} />)
-    : ( !userEmail && forgotten )
-    ? <ForgottenPassword
-        createChangeHandler
-        handleRecover />
-    : (<ResetPassword
-        createChangeHandler
-        handleReset
-        handleSubmitReset />)
-  )
-}
-
-function Signin({handleSubmit, createChangeHandler, handleForgotten}) {
-  const classes = useStyles()
-  return (
-    <Paper className={classes.paper}>
-      <form className={classes.form} onSubmit={handleSubmit}>
-        <TextField
-          autoFocus
-          fullWidth
-          id='username'
-          label='Username'
-          margin='normal'
-          name='username'
-          onChange={createChangeHandler('username')}
-          required variant='outlined' />
-
-        <TextField
-          fullWidth
-          id='password'
-          label='Password'
-          margin='normal'
-          name='password'
-          onChange={createChangeHandler('password')}
-          required autoComplete='no'
-          type='password'
-          variant='outlined' />
-
-        <Box component='div' className={classes.button}>
-          <Button className={classes.submit}
-            color='primary'
-            type='submit'
-            style={{'float': 'left'}}
-            variant='contained'>
-            Sign In
-          </Button>
-          <Button color='secondary'
-            onClick={handleForgotten}
-            style={{'float': 'right'}} variant='contained'>
-            RESET PASSWORD
-          </Button>
-
-          { /*<GoogleLoginButton className={ classes.submit } type='submit'/>*/ }
-        </Box>
-      </form>
-    </Paper>
-  )
+    <Fragment>
+      {renderView()}
+    </Fragment>
+   )
 }
 
 
