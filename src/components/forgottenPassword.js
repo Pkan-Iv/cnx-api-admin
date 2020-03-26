32e4fb8d242d075ae7d71fc60d6e2e097bb4d961
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-26 12:08:20 +0100

    Forgotten password view updated

--- src/components/forgottenPassword.js
+++ src/components/forgottenPassword.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
   Button,
@@ -6,48 +6,83 @@ import {
   TextField
 } from '@material-ui/core'
 
+import * as Config from '../../config.json'
+
 import { useStyles } from './auth'
 
 export default function ForgottenPassword({
-  createChangeHandler,
-  fields = {email:''},
-  forgotten = true,
-  handleForgotten,
-  handleRecover,
-  userEmail = false
+  handleforgotten,
+  valueforgotten,
+  valueuseremail,
 } = {}) {
-  const classes = useStyles()
+  const classes = useStyles(),
+  [ fields, setFields ] = useState({ email: '' })
 
-  return (<Paper className={classes.paper}>
-    <form className={classes.form}>
-      <TextField
-        autoFocus
-        fullWidth
-        id='email'
-        label='Email'
-        margin='normal'
-        name='email'
-        onChange={createChangeHandler('email')}
-        required
-        variant='outlined' />
+  function createChangeHandler(field) {
+    return (e) => {
+      setFields({ ...fields, [field]: e.target.value })
+    }
+  }
+  function handleRecover() {
+    const { server } = Config,
+      { HOST, PORT } = server,
+      { email } = fields,
+          url = `${HOST}:${PORT}/api/pra/reset`,
+          headersConfig = {
+            'Access-Control-Allow-Origin':'*',
+            'Content-Type': 'application/json',
+            Accept: `*/*`
+          },
+          request = { email: email },
+          data = {
+            headers: headersConfig,
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(request)
+          }
 
-      <Box component='div' className={classes.button}>
-        <Button className={classes.submit}
-          color='secondary'
-          onClick={handleForgotten}
-          style={{ 'float': 'left' }}
-          variant='contained'>
-          SIGN IN
-        </Button>
-        <Button
-         color='primary'
-         onClick={handleRecover}
-         style={{ 'float': 'right' }}
-         type='reset'
-         variant='contained'>
-          SEND EMAIL
-        </Button>
-      </Box>
-    </form>
-  </Paper>)
+    console.log('Fields: ',fields)
+    console.log('Body: ',data.body)
+    if (email !== undefined) {
+      fetch( url, data)
+      .then( (response) => response.json())
+      .then( (result) => console.log(result) )
+      .catch( (err) => console.log('error', err))
+    }
+  }
+
+  return (
+    <Paper className={classes.paper}>
+      <form className={classes.form}>
+        <TextField
+          autoFocus
+          fullWidth
+          id='email'
+          label='Email'
+          margin='normal'
+          name='email'
+          onChange={createChangeHandler('email')}
+          required
+          variant='outlined' />
+
+        <Box component='div' className={classes.button}>
+          <Button className={classes.submit}
+            color='secondary'
+            onClick={handleforgotten}
+            style={{ 'float': 'left' }}
+            variant='contained'>
+            SIGN IN
+          </Button>
+          <Button
+            color='primary'
+            onClick={handleRecover}
+            style={{ 'float': 'right' }}
+            type='reset'
+            variant='contained'>
+            SEND EMAIL
+          </Button>
+        </Box>
+      </form>
+    </Paper>
+  )
 }
