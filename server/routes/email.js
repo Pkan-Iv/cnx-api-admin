c1c66f2f20a2f3b06517c9ee6b48e20ab6a1a46e
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-26 14:14:13 +0100

    Get token route added

--- server/routes/email.js
+++ server/routes/email.js
@@ -4,5 +4,6 @@ import email from '../controller/email'
 const router = express.Router()
 
 router.post('/reset', email.getEmail)
+router.get('/reset/token', email.getEmailToken)
 
 export default router
