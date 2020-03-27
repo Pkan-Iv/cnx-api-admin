f0960870a349357ab32e59d6003ac997d33357e3
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-03-27 12:52:28 +0100

    Express-fileupload dependency added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import express from 'express'
+import fileUpload from 'express-fileupload'
 
 import Config from '../config.json'
 
 import email from './routes/email'
+import upload from './routes/upload'
 
 
 const app = express(),
@@ -20,7 +22,10 @@ app.use(
 )
 app.use( bodyParser.json() )
 
+app.use( fileUpload() )
+
 app.use('/api/pra', email)
+app.use('/api/pra', upload)
 
 app.listen(PORT, () =>
   console.log(`Server is listening on port ${PORT}`)
